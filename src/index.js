7b2e342c0c66297dae103c1421652a7433d88b94
Dina Shchobova <dina.shchobova@gmail.com>
2021-03-11 16:49:47 +0300

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = check;
+
+function check(str, bracketsConfig) {
+    let arr = str.split('');
+    let answer;
+    let stack = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        for (let k = 0; k < bracketsConfig.length; k++) {
+            if (arr[i] === bracketsConfig[k][0] && arr[i] === bracketsConfig[k][1]) {
+                if (stack[stack.length - 1] === bracketsConfig[k][1]) {
+                    stack.pop();
+                } else {
+                    stack.push(arr[i]);
+                }
+            } else {
+                if (arr[i] === bracketsConfig[k][0]) {
+                    stack.push(arr[i]);
+                } else if (arr[i] === bracketsConfig[k][1]) {
+                    if (!stack.length) {
+                        return false;
+                    }
+                    if (stack[stack.length - 1] === bracketsConfig[k][0]) {
+                        stack.pop();
+                    } else {
+                        return false;
+                    }
+
+                }
+
+            }
+
+        }
+    }
+    if (stack.length === 0) {
+        answer = true
+    } else (answer = false)
+    return answer;
+
 }
+
+//console.log(check('||', [['|', '|']]));
